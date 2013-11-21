f9e1a86e0c075080c462ae3fa95cba80cbd80624
edwinwow <yunlong.he@hotmail.com>
2013-11-21 20:53:12 +0800

    Update admin.py

--- admin.py
+++ admin.py
@@ -1,1 +1,12 @@
 from django.contrib import admin
+from cartridge.shop.admin import ProductAdmin
+from .models import Subscription, UserSubscription
+
+
+admin.site.register(Subscription, ProductAdmin)
+
+class UserSubscriptionAdmin(PageAdmin):
+    fieldsets = deepcopy(PageAdmin.fieldsets) 
+
+admin.site.register(UserSubscription, UserSubscriptionAdmin)
+
